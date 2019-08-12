47a8e728554457b71d6a85bf5d9abb8d5da90091
Thomas Krieger <thomas.krieger@puppet.com>
2019-08-12 14:02:41 +0200

    added test parameter

--- spec/classes/sshkeymgmt_spec.rb
+++ spec/classes/sshkeymgmt_spec.rb
@@ -4,6 +4,13 @@ describe 'sshkeymgmt' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:params) do
+        {
+          'users' => {},
+          'groups' => {},
+          'ssh_key_groups' => {},
+        }
+      end
 
       it { is_expected.to compile }
     end
